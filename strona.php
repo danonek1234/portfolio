15f14b9c49209977505f287e2742ef9d14620d66
danonek1234 <oliwier.patyk2006@gmail.com>
2023-09-07 08:46:15 +0200

    Update strona.php

--- strona.php
+++ strona.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
     <title>Portfolio</title>
 </head>
 <body>
-    
+    <h1>Portfolio</h1>
+    <div class="section_one">
+        
+    </div>
 </body>
 </html>
